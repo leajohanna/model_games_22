16bd2136481c68a48b254f404c5bfc30fcf8dfeb
leajohanna <lea.zucker@edu.fh-joanneum.at>
2023-04-25 17:26:35 +0200

    ad more stuff

--- Assets/look_at_fixed.cs
+++ Assets/look_at_fixed.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_die : MonoBehaviour
+public class look_at_fixed : MonoBehaviour
 {
-    public float threshhold;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
